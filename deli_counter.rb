626e405c8ae4c55323ab1453521e417088445920
Zachary Lynch <zachary.lynch1337@gmail.com>
2020-05-09 18:49:31 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,12 @@
 # Write your code here.
+katz_deli = []
+
+def line(array)
+  if array.size == 0 
+    return puts "The line is currently empty."
+end
+
+def take_a_number(array, person)
+  array << person
+  return array
+  
